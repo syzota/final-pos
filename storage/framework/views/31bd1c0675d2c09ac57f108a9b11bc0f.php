fdc2a09932ff00d0eaae2ab5ecbb90a6d4d2dd36
dani <ad480132@gmail.com>
2026-08-30 11:39:45 +0800

    ci: setup ci/cd pipeline menggunakan github action

---
+++ storage/framework/views/31bd1c0675d2c09ac57f108a9b11bc0f.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+
+    <title>Posyandu Loa Duri Ulu</title>
+
+    <link rel="icon" type="image/png" href="<?php echo e(asset('image.png')); ?>?v=2">
+    <link rel="apple-touch-icon" href="<?php echo e(asset('image.png')); ?>?v=2">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
+        rel="stylesheet"
+    >
+
+    <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/main.jsx'); ?>
+</head>
+
+<body>
+    <div id="root"></div>
+</body>
+</html><?php /**PATH /app/resources/views/app.blade.php ENDPATH**/ ?>
